fdebbf3aa23f0d73b4cb5f5db4c1f42dfd3ee615
TheBharatMishra <mishrabm2004@gmail.com>
2024-02-19 13:32:54 +0530

    Server Up

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+import express from "express";
+import "dotenv/config";
+
+const app = express();
+const PORT = process.env.PORT || 6969;
+
+app.get("/", (req, res) => {
+  res.send("Server is Ready!");
+  console.log(req);
+});
+app.listen(PORT, () => console.log(`Listening at ${PORT}`));
