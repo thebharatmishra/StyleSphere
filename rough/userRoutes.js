791dba31ae51837f4bde4e466bf48bb4c20aecda
TheBharatMishra <mishrabm2004@gmail.com>
2025-01-25 23:07:47 +0530

    changes

--- rough/userRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { authUser } from "../controllers.js/userController.js";
-const router = express.Router();
-
-router.post("/auth", authUser);
-
-export default router;
