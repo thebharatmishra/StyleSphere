297adf48f9031529ab9a06fc8900065314df5f35
TheBharatMishra <mishrabm2004@gmail.com>
2025-01-29 15:21:35 +0530

    contact us

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -3,7 +3,15 @@ import React from "react";
 function Home() {
   return (
     <div className="container">
-      <h1>Welcome to Our E-commerce Store</h1>
+      <h1>Style Sphere</h1>
+      <h2>Welcome to Our E-commerce Store</h2>
+      <h3>
+        For further details contact us at{" "}
+        <a href="mailto:bharatmishrawork@yahoo.com">
+          bharatmishrawork@yahoo.com
+        </a>
+      </h3>
+
       <div className="products">{/* Products will be mapped here later */}</div>
     </div>
   );
